71742153e8faba2fab312b8e6ef5c23f49da1915
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-07-28 23:33:19 +0800

    Upload files

---
+++ precache-manifest.dd27287828514f154d4fe84eb5d4f117.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "4701b99de6b91d4241082edcb7a801a6",
+    "url": "fonts/noto-sans-sc-v9-chinese-simplified-regular.eot"
+  },
+  {
+    "revision": "81421dd5cb5cf2865ae7ce4d4ef5bda3",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.eot"
+  },
+  {
+    "revision": "0e58c6ea31d3a21e6d3f",
+    "url": "js/chunk-vendors.0b3b5c57.js"
+  },
+  {
+    "revision": "f9db37036bc11b40bdf56e782aa3de83",
+    "url": "img/discard.f9db3703.png"
+  },
+  {
+    "revision": "4c3a67e47cf71d08bb1376a8e5b6be6a",
+    "url": "img/save.4c3a67e4.png"
+  },
+  {
+    "revision": "777f271aa7856f755af4ace36a6f201f",
+    "url": "index.html"
+  },
+  {
+    "revision": "44d80edbb024d08c79042218e7277f68",
+    "url": "fonts/noto-sans-sc-v9-chinese-simplified-regular.svg"
+  },
+  {
+    "revision": "38c2427bf41fab58dc15846f42ac9a77",
+    "url": "fonts/noto-sans-sc-v9-chinese-simplified-regular.woff"
+  },
+  {
+    "revision": "44d80edbb024d08c79042218e7277f68",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.svg"
+  },
+  {
+    "revision": "f3d391eb1981867a2727",
+    "url": "js/app.7bec1528.js"
+  },
+  {
+    "revision": "37e9b51a7f48e1d9e189ead36be9cd5f",
+    "url": "img/icon192.png"
+  },
+  {
+    "revision": "c2c0e68c131e9bfb90c619ca577d8fee",
+    "url": "img/welcome.png"
+  },
+  {
+    "revision": "3f17684518c5891a47e9c833ddfd9310",
+    "url": "img/icon512.png"
+  },
+  {
+    "revision": "5d98112e90b726aee70b63e6b0ca0c66",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.woff2"
+  },
+  {
+    "revision": "94df89dfde0c0cec28a297dfa542c6b1",
+    "url": "fonts/noto-sans-sc-v9-chinese-simplified-regular.woff2"
+  },
+  {
+    "revision": "1e99b3cf2a08a6c12357e173b632c962",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.woff"
+  },
+  {
+    "revision": "9bac1a26f87467e164b35db750dc57c8",
+    "url": "css/googlefonts.css"
+  },
+  {
+    "revision": "0e58c6ea31d3a21e6d3f",
+    "url": "css/chunk-vendors.3c032826.css"
+  },
+  {
+    "revision": "f3d391eb1981867a2727",
+    "url": "css/app.66dcf6f4.css"
+  }
+];
